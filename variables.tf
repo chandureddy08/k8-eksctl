7c344f9254567b2641dcb5596b0ae8b8d1d43771
chandureddy08 <chandrareddyerigela@gmail.com>
2024-07-09 17:31:06 -0400

    shipping

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "sg_id" {
+  type        = string
+  default     = "sg-0ad74b957b6b7351f"
+  description = "description"
+}
+
+variable "public_subnet_id" {
+    type = string
+    default = "subnet-02438e2af84b2481e"
+}
